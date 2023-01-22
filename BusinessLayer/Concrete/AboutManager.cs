e9f0634b8a0932480a7cace468c6a3326aafe331
Hamza Irmak <hamzairmak46tr@gmail.com>
2023-01-22 09:57:07 +0300

    manager about add

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutDal.insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.delete(t);
+        }
+
+        public About TGetById(int id)
+        {
+            return _aboutDal.GetById(id);
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutDal.GetAll();
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.update(t);
+        }
+    }
+}
